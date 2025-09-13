de995a3a49e8df973441e2f5a145b4fcfc9d3013
Michael Buckley <michaelcbuckley@proton.me>
2025-09-13 10:16:02 -0400

    Tweaked packages and added some comments

--- config/nixos/common.nix
+++ config/nixos/common.nix
@@ -1,14 +1,20 @@
 # A common config base to be shared among all project VMs
 {pkgs, ...}: {
+  # I use unfree but this may be changed, which may require users handle accordingly
   nixpkgs.config.allowUnfree = true;
+  # This is just for convenience sake
   environment.enableAllTerminfo = true;
-  system.stateVersion = "25.05";
+
+  system.stateVersion = "25.11";
 
   environment.systemPackages = with pkgs; [
+    curl
+    gitMinimal
     microfetch
     neovim
   ];
 
+  # Some useful defaults
   nix = {
     package = pkgs.nixVersions.latest;
     settings = {
